42932cbb1d9551b421b6b3c76b52fbcd50f871c6
Egor Ponomarev <egor@ponomarev.pp.ru>
2018-04-18 12:44:11 +0300

    user card

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const morgan = require('morgan')
 
 require('./app_api/models/db');
 
@@ -8,16 +9,20 @@ const routeApi = require('./app_api/routes/index')
 
 const app = express();
 
+app.use(morgan('combined'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'app_client')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use('/api', routeApi);
 
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
 });
+
+
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- app_client/app.js
+++ app_client/app.js
@@ -1,19 +1,7 @@
 angular
-	.module('DroneCafeApp', ['ngRoute', 'ui.router']) // , 'auth' , 
+	.module('DroneCafeApp', [ 'ui.router']) // , 'auth' , 
 	.controller('droneCtrl', droneCtrl)
 
-	.config(['$routeProvider', '$stateProvider', function ($routeProvider, $stateProvider) {
-		$stateProvider
-			.state('client', {
-				url: "/",
-				templateUrl: "client/client.view.html",
-				controller: "clientCtrl as vm"
-			})
-			.state('kitchen', {
-				url: "/kitchen",
-				templateUrl: "kitchen/kitchen.view.html"
-			})
-	}]);
 
 function droneCtrl($scope) {
 	$scope.email="m@m.ru";
